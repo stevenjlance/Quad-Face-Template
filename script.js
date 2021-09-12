c312d65a74f50f5463c912352003509767bb84cf
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-09-12 21:42:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(220);
+  text(mouseX + ", " + mouseY, 20,20)
+
+  //base face shape
+  quad(30,350,100,30,280,30,350,350)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
